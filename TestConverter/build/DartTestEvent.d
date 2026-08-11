f1a9e0a0c6312f4462da23ee67c6b4b99c4f14a8
Sarah Smith <sez@storybridge.org>
2026-08-11 22:22:42 +1000

    chown build dir, remove build artifacts

--- TestConverter/build/DartTestEvent.d
+++
@@ -1,6 +0,0 @@
-build/DartTestEvent.o: src/DartTestEvent.cpp include/DartTestEvent.h \
-  include/nlohmann/json.hpp include/Test.h include/Metadata.h
-include/DartTestEvent.h:
-include/nlohmann/json.hpp:
-include/Test.h:
-include/Metadata.h:
--- TestConverter/build/main.d
+++
@@ -1,6 +0,0 @@
-build/main.o: src/main.cpp include/nlohmann/json.hpp \
-  include/DartTestEvent.h include/Test.h include/Metadata.h
-include/nlohmann/json.hpp:
-include/DartTestEvent.h:
-include/Test.h:
-include/Metadata.h:
